e5df5d6675178c5099afd69773cf594eea354a8a
koon95 <kentchickboy@yahoo.com>
2023-09-20 15:16:57 +0800

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,15 @@
+#slicing = create a substring by extracting elements from another string indexing [] or slice()
+# [start:stop:step]
+
+name = "Bro Code"
+
+
+first_name = name[:3]
+last_name = name[4:]
+funky_name = name[::3]
+reversed_name = name[::-1]
+
+print(first_name)
+print(last_name)
+print(funky_name)
+print(reversed_name)
