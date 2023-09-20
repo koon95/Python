6fc7e4e41daea7769b2e37b5324b24b29ce75bfa
koon95 <kentchickboy@yahoo.com>
2023-09-20 15:25:43 +0800

    Add files via upload

---
+++ slicing function.py
@@ -0,0 +1,12 @@
+#slicing = create a substring by extracting elements from another string
+# indexing[] or slice []
+#  [start:stop:step]
+
+website1 = "https://google.com"
+website2 = "https://wikipedia.com"
+
+slice = slice(8,-4)
+
+
+print(website1[slice])
+print(website2[slice])
