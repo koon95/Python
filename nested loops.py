10db9be7ed584edb7c3bb208f4bc38d61f621c7e
koon95 <kentchickboy@yahoo.com>
2023-09-21 19:14:06 +0800

    Add files via upload

---
+++ nested loops.py
@@ -0,0 +1,23 @@
+#nested loops = the "inner loop" will finish all of its iterations before
+#               finishing one iteration of the "outer loop"
+
+
+rows = int(input("How many rows?: "))
+columns = int(input("How many columns?: "))
+symbol = input("Enter a symbol: ")
+
+for i in  range(rows):
+    for j in range(columns):
+        print(symbol, end="")
+    print()
+
+
+
+
+
+
+
+
+
+
+
